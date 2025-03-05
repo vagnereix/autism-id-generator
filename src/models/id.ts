a03d47116bd388e443cdbc237de3cf768b7f9d26
Vagner <vagnereix.dev@gmail.com>
2025-03-05 13:00:19 -0300

    feat: adds id model

---
+++ src/models/id.ts
@@ -0,0 +1,16 @@
+export type Identification = {
+    fullName: string
+    firstContact: {
+        name: string
+        phone: string
+    }
+    secondContact: {
+        name: string
+        phone: string
+    }
+    address: string
+    diagnosticLink: string
+    additionalInfo: string
+    cid: string
+    dateOfBirth: string
+}
