b359aa856a3e1b65f02547958840b3bb8e2a04f4
Vagner <vagnereix.dev@gmail.com>
2025-03-06 14:46:36 -0300

    refactor: styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased font-sans overflow-x-hidden relative`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased font-sans overflow-x-hidden relative bg-zinc-950 dark`}
       >
         {children}
       </body>
